3b6ff039de26f0726f0ceb2523c611b2873009ba
Claudia Petty <claudialove32@gmail.com>
2020-04-13 19:17:17 -0400

    first push with pseudo-code and read-me

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # weather-app
+
+App Title: 
+Weather Dashboard
+
+App Type:
+search and display data
+
+App Description:
+--This app allows you to search for any valid city and see todays weather and a five day forecast
+--This app will save your previous searches so you can check the weather there with on click
+
---
+++ script.js
@@ -0,0 +1,6 @@
+//this app should call the weather api on load
+//this app should check local storage to show saved searches if they exist
+//this app should display saved searches if they exist
+//when search is used this app should validate city selection
+//when a valid city is selected this app should be populated with todays weather data and the 5 day forecast
+
